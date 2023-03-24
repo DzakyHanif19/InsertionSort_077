4b403778aeac723a06270fd1fde170f027e8d3b1
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-03-24 15:44:13 +0700

    membuat inputan jumlah dan menampilkan pesan

--- Insertion Sort/Insertion Sort.cpp
+++ Insertion Sort/Insertion Sort.cpp
@@ -4,4 +4,18 @@ using namespace std;
 int arr[20]; // membuat array dengan panjang data 20
 int n; //membuat variabel inputan n
 
-void input(){}
+void input() {
+	while (true)
+	{
+		cout << "masukan jumlah data pada array : ";
+		cin >> n;
+
+		if (n <= 20) {
+			break;
+		}
+		else
+		{
+			cout << "\n array yang anda masukan maksimal 20 elemen. \n";
+		}
+	}
+}
